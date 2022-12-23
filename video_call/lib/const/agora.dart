139e398870c08d9fdabe7fb6f9e719f865a5f009
HoooDev <lgh3806@naver.com>
2022-12-23 16:04:12 +0900

    flutter video_call

---
+++ video_call/lib/const/agora.dart
@@ -0,0 +1,4 @@
+const APP_ID = 'e0c8f8b06abd4d96af75cd1379622372';
+const CHANNEL_NAME = 'ghChannel';
+const TEMP_TOKEN =
+    '007eJxTYDi1a8mDPSuqlRtz3X+6PeWz3v5/5epXazfbHhI4ZXW8WmeSAkOqQbJFmkWSgVliUopJiqVZYpq5aXKKobG5pZmRkbG5kRr70uSGQEaGEv8VzIwMEAjiczKkZzhnJOblpeYwMAAAxgQi+A==';
